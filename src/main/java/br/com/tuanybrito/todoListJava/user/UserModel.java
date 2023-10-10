f4eeef810a9fb2b340ed1e6ed3333761197a2bb5
Santos <tuany.a.santos@accenture.com>
2023-10-09 23:42:26 -0300

    init spring boot project

---
+++ src/main/java/br/com/tuanybrito/todoListJava/user/UserModel.java
@@ -0,0 +1,7 @@
+package br.com.tuanybrito.todoListJava.user;
+
+public class UserModel {
+    public String userName;
+    public String name;
+    public String password;
+}
---
+++ src/test/java/br/com/tuanybrito/todoListJava/TodoListJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tuanybrito.todoListJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
